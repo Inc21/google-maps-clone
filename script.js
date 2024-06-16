9b604571e356592d0924005f57396b2d2f06aad7
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2024-06-16 23:35:40 +0100

    sign up for mapbox and get all the files set up

---
+++ script.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaW5jMjEiLCJhIjoiY2x4aTNqNXZ0MTcwNjJqczZ0a3RuNHJxayJ9.HPfO3roDOgkrwJFg-hDQog';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+  });
